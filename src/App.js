1b434af4e04349f6f8591fc6b26884b11fafa586
Tomson Kan <tomson.kan@gmail.com>
2020-01-16 16:13:01 -0600

    Final upload with comments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Details from './Details';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//isLoaded False since array has not been populated
+class App extends Component {
+  constructor(props) {
+      super (props) 
+        this.state = {
+          isLoaded: false,
+          users: [],
+          uuid: []
+        }
+  }
 
-export default App;
+
+  //fetches and loads list 
+  componentDidMount() {
+    fetch('https://randomuser.me/api?results=25')
+    .then(res => res.json())
+    .then(json => {
+        this.setState({
+          isLoaded: true,
+          users: json.results,
+        })
+    })
+    .catch(error => console.log('parsing failed'. error))
+  }
+
+  
+//calling Details function from Details.js file
+  render() {
+    console.log(this.state);
+    return (
+      <>
+      {this.state.users.map(person => (
+      <div className="App" key ={person.login.uuid}>
+              <Details person = {person}/>
+      </div>
+            ))}
+      </>
+    );
+  }
+}
+  export default App;
+
---
+++ src/Details.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react'
+
+class Details extends Component {
+  state = {
+    toggle: false,
+    show: true
+  }
+
+//function displays detail once button is pressed
+handleDetail = () => {
+  const newState = !this.state.toggle;
+  // const newInfo = !this.state.show;
+  this.setState({
+    toggle: newState,
+    show: !this.state.show
+  })
+  console.log("**This state**", this.state.toggle)
+}
+
+render() {
+
+  const {toggle} = this.state;
+  const textValue = toggle ? "Hide Detail" : "Show Detail";
+  const buttonColor = toggle ? "blackButton" : "whiteButton";
+
+  return <div>
+    <h1>{this.props.person.name.first} {this.props.person.name.last} </h1>
+              <img src = {this.props.person.picture.large} alt = "undefined"></img>
+      <button 
+        onClick={() => this.handleDetail()}
+        style={{margin:5, flex:1,height:60,backgroundColor:buttonColor,justifyContent:'center'}}>
+        {textValue}
+      </button>
+      {!this.state.show && <Child person = {this.props.person}/>}
+  </div>
+}
+}
+
+//created Child formatted to put in JSX
+const Child = ({person}) => {
+  console.log("***check***", person)
+  let {street, city, state, country} = person.location
+  return <>
+  
+  <div className='addlInfo' >
+  <ul>
+  <li>{street.number} {street.name} 
+  <p>{city} {state} {country}  </p>
+  </li>
+  <li>
+    { person.email}
+  </li>
+  </ul>
+    </div>
+  </>
+}
+
+
+  export default Details
